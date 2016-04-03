d13dc22480034d4c718abaae55b67053c71233bd
Ayushi <ayushimahajan05@gmail.com>
2016-04-03 13:48:00 +0100

    Add solution for problem `A - Word Capitalization` Link: https://codeforces.com/contest/281/problem/A Tags: implementation, strings, *800 Ref: https://codeforces.com/contest/281/submission/17145118

--- codeforces/281/A.java
+++ codeforces/281/A.java
@@ -1,20 +1,13 @@
 import java.util.Scanner;
 
 /**
- * Created by Ayushi on 2/15/2018.
+ * Created by mahaa on 03/04/2016.
  */
-public class wordCapitalization281A {
-    public static void main(String[] args) {
+public class WordCapitalization281A {
+    public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        int l = input.length();
-        String[] c = input.split("");
-        c[0] = c[0].toUpperCase();
-        String ans = "";
-
-        for (int i = 0; i < l; i++) {
-            ans = ans + c[i];
-        }
-        System.out.println(ans);
+        String input = scan.next();
+        String output = input.substring(0, 1).toUpperCase() + input.substring(1);
+        System.out.println(output);
     }
 }
