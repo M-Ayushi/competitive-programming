e2607cc25dbf9fe077ca4248bab8b3ba35a9eaa7
Ayushi <ayushimahajan05@gmail.com>
2018-06-11 15:41:00 +0100

    Add solution for problem `D - I'm Feeling Lucky!` Link: https://codeforces.com/contest/952/problem/D Tags: probabilities, *1800 Ref: https://codeforces.com/contest/952/submission/39161710

---
+++ codeforces/952/D.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Created by Ayushi on 4/7/2018.
+ */
+public class CF_952D_ImFeelingLucky {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int in = scan.nextInt();
+        scan.close();
+        System.out.println("Red");
+    }
+}
+
+	 		  	 	 
+	   	  		 
+		 		 				
+	  	 	    
