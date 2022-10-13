0acfbe26383db29785a63d0a5319da59886e73b4
Ayushi <ayushimahajan05@gmail.com>
2022-10-13 15:57:00 +0100

    Add solution for problem `B - Increasing` Link: https://codeforces.com/contest/1742/problem/B Tags: greedy, implementation, sortings, *800 Ref: https://codeforces.com/contest/1742/submission/175939626

---
+++ codeforces/1742/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x).size()
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	int a[n];
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	sort(a, a+n);
+	string ans = "YES\n";
+	for (int i = 1; i < n; ++i) {
+		if (a[i] == a[i-1]) {
+			ans = "NO\n";
+			break;
+		}
+	}
+	cout << ans;
+	return;
+}
+ 
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) solve();
+	
+    return 0;
+}
