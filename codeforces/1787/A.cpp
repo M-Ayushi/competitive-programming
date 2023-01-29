cfa2cfc58526039a2ba889d6d12381dea702c998
Ayushi <ayushimahajan05@gmail.com>
2023-01-29 14:42:00 +0000

    Add solution for problem `A - Exponential Equation` Link: https://codeforces.com/contest/1787/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/1787/submission/191111668

---
+++ codeforces/1787/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x.size())
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	if (n % 2 == 0) cout << (n/2) << " 1\n";
+	else cout << "-1\n";
+	
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
