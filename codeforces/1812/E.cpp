402c9cff0948430e9e4034b05c2181d866769414
Ayushi <ayushimahajan05@gmail.com>
2023-04-01 15:58:00 +0100

    Add solution for problem `E - Not a Geometry Problem` Link: https://codeforces.com/contest/1812/problem/E Tags: *special problem, *special problem, constructive algorithms, geometry, math Ref: https://codeforces.com/contest/1812/submission/200164660

---
+++ codeforces/1812/E.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x.size())
+using namespace std;
+
+void solve() {
+	int x, y, z;
+	cin >> x >> y >> z;
+
+	cout << "0\n";
+	
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	
+	return 0;
+}
