7829da5082eb8eca78fc7ed3af7ba06c285583bf
Ayushi <ayushimahajan05@gmail.com>
2023-04-29 16:06:00 +0100

    Add solution for problem `B - Indivisible` Link: https://codeforces.com/contest/1818/problem/B Tags: constructive algorithms, *900 Ref: https://codeforces.com/contest/1818/submission/203926014

---
+++ codeforces/1818/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x.size())
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	if (n == 1) cout << "1\n";
+	else if (n % 2) cout << "-1\n";
+	else {
+		for (int i = 1; i <= n; i+=2) {
+			cout << (i+1) << " " << i << " ";
+		}
+		cout << "\n";
+	}
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
