300333d51f37ec3c4391a12ce0c1aad92ab6ea1a
Ayushi <ayushimahajan05@gmail.com>
2023-07-11 15:37:00 +0100

    Add solution for problem `A - Subtraction Game` Link: https://codeforces.com/contest/1844/problem/A Tags: constructive algorithms, games, *800 Ref: https://codeforces.com/contest/1844/submission/213299565

---
+++ codeforces/1844/A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define sz(x) (int)(x.size())
+using namespace std;
+
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	cout << (a+b) << "\n";
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
